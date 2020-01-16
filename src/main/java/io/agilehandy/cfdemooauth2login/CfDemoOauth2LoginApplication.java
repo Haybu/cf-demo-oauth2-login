c27e5834ab4a48fdf18f89e11684763d6b153788
Haytham Mohamed <haybu@hotmail.com>
2020-01-16 11:29:52 -0500

    initial

---
+++ src/main/java/io/agilehandy/cfdemooauth2login/CfDemoOauth2LoginApplication.java
@@ -0,0 +1,15 @@
+package io.agilehandy.cfdemooauth2login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@SpringBootApplication
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class CfDemoOauth2LoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfDemoOauth2LoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/agilehandy/cfdemooauth2login/CfDemoOauth2LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package io.agilehandy.cfdemooauth2login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfDemoOauth2LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
